78560d2ab23f150c721ceefc3aaa7ebcdd4a30f8
Paul Beard <pbeardey@gmail.com>
2022-05-24 21:38:13 +0100

    Step 1

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+
+    return (
+        <header>
+            <h1>My First React Project</h1>
+            <div>my name</div>
+        </header>
+    );
+}
+
+export default Header;
