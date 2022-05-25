c35f23bc6b4fe77283fabe9866ce032f371af731
Paul Beard <pbeardey@gmail.com>
2022-05-25 20:11:18 +0100

    step  3

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
 const Dropdown = ( { title, options } ) => {
+    const isOpen = false;
+
     return (
-        <div>
-          <div>{title}</div>
-          <div>
-            <ul>
-              {options.map( (item, i) => (
-                  <li key={i}>{item}</li>
-                ))}
-            </ul>
-          </div>
-        </div>
+      <div>
+        <div>{title}</div>
+            { 
+            isOpen && (
+                <div>
+                  <ul>
+                    {options.map( (item, i) => (
+                        <li key={i}>{item}</li>
+                      ))}
+                  </ul>
+                </div>
+            )
+            }
+      </div>
     );
 }
 
